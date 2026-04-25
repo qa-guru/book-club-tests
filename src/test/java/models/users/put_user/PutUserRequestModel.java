053b4e865c0e0830b39f9fd883864b978de06452
Viktor Korol <v1ck1ng1703@yandex.ru>
2026-04-25 15:12:49 +0300

    finished put_user negative tests

--- src/test/java/models/users/put_user/PutUserRequestModel.java
+++ src/test/java/models/users/put_user/PutUserRequestModel.java
@@ -1,6 +1,20 @@
 package models.users.put_user;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
+
+@JsonInclude(NON_NULL)
 public record PutUserRequestModel(String username,
                                   String firstName,
                                   String lastName,
-                                  String email) {}
+                                  String email) {
+
+    public static PutUserRequestModel withoutFirstName(String username, String lastName, String email) {
+        return new PutUserRequestModel(username, null, lastName, email);
+    }
+
+    public static PutUserRequestModel withoutAllFields() {
+        return new PutUserRequestModel(null, null, null, null);
+    }
+}
--- src/test/java/tests/UpdateUserTests.java
+++ src/test/java/tests/UpdateUserTests.java
@@ -3,11 +3,13 @@ package tests;
 import models.auth.login.LoginRequestModel;
 import models.users.UserSuccessResponseModel;
 import models.users.put_user.PutUserRequestModel;
+import models.users.put_user.PutUserValidationErrorResponseModel;
 import models.users.registration.RegistrationRequestModel;
 import net.datafaker.Faker;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
+import static tests.TestData.REQUIRED_FIELD_ERROR;
 
 public class UpdateUserTests extends TestBase {
 
@@ -52,7 +54,33 @@ public class UpdateUserTests extends TestBase {
         LoginRequestModel loginData = new LoginRequestModel(registrationData.username(), registrationData.password());
         String accessToken = api.auth.loginAndGetAccessToken(loginData);
 
-        PutUserRequestModel updateData = new PutUserRequestModel(username, lastName, email);
-        UserSuccessResponseModel updateResponse = api.users.putUser(accessToken, updateData);
+        PutUserRequestModel updateData = PutUserRequestModel.withoutFirstName(username, lastName, email);
+
+        PutUserValidationErrorResponseModel response = api.users.validationErrorPutUser(accessToken, updateData);
+        String expectedErrorMessage = REQUIRED_FIELD_ERROR;
+
+        assertThat(response.firstName()).containsExactly(expectedErrorMessage);
+        assertThat(response.username()).isNull();
+        assertThat(response.lastName()).isNull();
+        assertThat(response.email()).isNull();
+    }
+
+    @Test
+    public void withoutAllFieldsFailedUpdateUserTest() {
+        RegistrationRequestModel registrationData = new RegistrationRequestModel(username, password);
+        api.users.register(registrationData);
+
+        LoginRequestModel loginData = new LoginRequestModel(registrationData.username(), registrationData.password());
+        String accessToken = api.auth.loginAndGetAccessToken(loginData);
+
+        PutUserRequestModel updateData = PutUserRequestModel.withoutAllFields();
+
+        PutUserValidationErrorResponseModel response = api.users.validationErrorPutUser(accessToken, updateData);
+        String expectedErrorMessage = REQUIRED_FIELD_ERROR;
+
+        assertThat(response.firstName()).containsExactly(expectedErrorMessage);
+        assertThat(response.username()).containsExactly(expectedErrorMessage);
+        assertThat(response.lastName()).containsExactly(expectedErrorMessage);
+        assertThat(response.email()).containsExactly(expectedErrorMessage);
     }
 }
